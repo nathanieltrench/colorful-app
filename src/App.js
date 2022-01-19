6b318e78a3db5c03eb68392aabdec8a6e976a4a9
Nate Trench <nate.trench@appcues.com>
2022-01-19 13:59:40 -0600

    css one

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ const App = () => {
 
   window.Appcues.identify(userId, { // feel free to add in any user properties here
     role: "tester",
-    signupDate: currentTime
+    signupDate: currentTime,
+    company: "WED Company",
+    firstName: "Walter",
+    lastName: "Elias"
   })
 
   window.Appcues.on("all", function(name, payload) {
